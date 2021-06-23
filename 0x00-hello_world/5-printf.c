8d69dca2dfdec19ded70cf9463b9c8c4700a3494
Niqualis <you@example.com>
2021-06-23 16:39:43 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *main - Uses printf to print a string
+ *
+ *Description: use printf
+ *Return: Should be zero (0)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
