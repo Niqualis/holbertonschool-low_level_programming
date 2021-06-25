7be6b9b9145ca407e663a890b35cb4dd5167c37a
niqualis <NicholasJWallace@protonmail.com>
2021-06-25 11:57:02 -0700

    negpos

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 
-/*main - randomizing positive and negative numbers  */
+/**
+* main - Prints if number is positive, zero or negative
+*
+* Return: Always (Success)
+*/
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if (n > 0)
 {
 printf("%d is positive\n", n);
@@ -21,5 +26,6 @@ else
 {
 printf("%d is negative\n", n);
 }
+
 return (0);
 }
