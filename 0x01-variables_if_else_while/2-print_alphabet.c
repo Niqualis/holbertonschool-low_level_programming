2d9a1fa64895e28dd1be828d797324964491eff1
niqualis <NicholasJWallace@protonmail.com>
2021-06-25 17:26:16 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - returns 0
+*
+* putchar prints lowercase alphabet
+* Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
