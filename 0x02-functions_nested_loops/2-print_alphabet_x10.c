d4ec501fdb5270d8aa408f777adc9f99e860e8b6
niqualis <NicholasJWallace@protonmail.com>
2021-06-28 19:36:24 -0700

    alphax10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* print_alphabet_x10 - takes unspecificed number of arguments
+* Description: 10 times the alphabet in lowercase
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+int num;
+int il;
+
+for (num = 0; num <= 9; num++)
+{
+for (il = 'i'; il <= 'i'; il++)
+{
+_putchar(il);
+}
+_putchar('\n');
+}
+}
