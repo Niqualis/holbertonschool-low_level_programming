62d3c1dff3bdfbb03c40cf281d31ad38e1421f79
niqualis <NicholasJWallace@protonmail.com>
2021-07-06 13:15:35 -0700

    multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* mul - Multiply two numbers
+*
+* Description: Multiply two numbers
+* @a: number 1
+* @b: number 2
+* Return: Integer
+*
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
