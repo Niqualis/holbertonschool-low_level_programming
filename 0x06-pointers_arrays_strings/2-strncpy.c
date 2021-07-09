a1a9e869e1ca2e0756f179c71b2c4f5c41f009bf
niqualis <NicholasJWallace@protonmail.com>
2021-07-09 12:31:57 -0700

    function-copy-string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,21 @@
+#include "holberton.h"
+
 /**
-* _strncpy - copies string, up to n bytes
-*
-* @dest: destination str to copy into
-* @src: string to copy to dest
-* @n: number of bytes to copy from src
-*
-* Return: char pointer to beginning of dest
+* _strncpy - a function that copies a string
+* @dest: copy to
+* @src: copy from
+* @n: input number of char
+* Return: Always 0 (Success)
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-char *temp = dest;
-int counter = 0, srcLength = 0;
+int i;
 
-if (n <= 0)
+for (i = 0; src[i] != '\0'; i++)
+if (i < n)
+dest[i] = src[i];
+while (i < n)
+dest[i++] = '\0';
+
 return (dest);
-while (src[srcLength] != '\0')
-srcLength++;
-srcLength++;
-if (n > srcLength)
-n = srcLength;
-
-while (counter < n)
-{
-dest[counter] = src[counter];
-counter++;
-}
-return (temp);
 }
